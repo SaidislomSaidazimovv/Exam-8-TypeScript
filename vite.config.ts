2725af3a9e76c59576a65328180ec6db3ccca064
SaidislomSaidazimovv <saidazimovsaidislom97@gmail.com>
2024-10-08 20:35:05 +0500

    njf

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Agar maxsus yo'l bo'lsa, masalan '/my-project/'
 })
