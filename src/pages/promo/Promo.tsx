f199f40099a4269c42bcca25d24f4fb1354a3948
SaidislomSaidazimovv <saidazimovsaidislom97@gmail.com>
2024-10-09 20:14:27 +0500

    errorr fixed

--- src/pages/promo/Promo.tsx
+++ src/pages/promo/Promo.tsx
@@ -1,27 +1,37 @@
 import { useState, useEffect } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import slide1 from "../../assets/slide1.jpg"
+import slide2 from "../../assets/slide2.jpg"
+import slide3 from "../../assets/slide3.jpg"
+import slide4 from "../../assets/slide4.jpg"
+import slide5 from "../../assets/slide5.jpg"
 
 const brands = [
   {
     name: "OLAPLEX",
     description: "Сила профессионального ухода для безупречных волос",
+    image: slide1,
   },
   {
     name: "MAREVE",
     description: "Скидка 20% на набор",
+    image: slide2,
   },
   {
     name: "NaturalME",
     description:
       "При покупке любого продукта NaturalME - крем Derma в подарок!",
+    image: slide3,
   },
   {
     name: "Paloma",
     description: "При покупке двух товаров Paloma Cosmetics третий в подарок",
+    image: slide4,
   },
   {
     name: "tanita",
     description: "При покупке двух товаров марки tanita третий в подарок!",
+    image: slide5,
   },
 ];
 
@@ -65,7 +75,7 @@ const Promo = () => {
           }`}
         >
           <img
-            // src={brands[currentIndex].image}
+            src={brands[currentIndex].image}
             alt={brands[currentIndex].name}
             className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
           />
